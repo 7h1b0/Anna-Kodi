bb7239988dd09802fcc3f1223af8709251443499
7h1b0 <wyrm1992@hotmail.fr>
2015-09-06 19:53:03 +0200

    Define devices to Power on and off

--- addon.py
+++ addon.py
@@ -17,27 +17,39 @@ class Anna(xbmc.Player):
 
 	def onPlayBackStarted(self):
 		if playlist.getposition() == 0:
-			deviceid = settings.getSetting("deviceid_start")
-			self.request(deviceid, False)
+			deviceid_off = settings.getSetting("deviceid_start_on")
+			self.request(deviceid_off, True)
+			deviceid_on = settings.getSetting("deviceid_start_off")
+			self.request(deviceid_on, False)
 
 	def onPlayBackPaused(self):
-		deviceid = settings.getSetting("deviceid_pause")
-		self.request(deviceid,True)
+		deviceid_on = settings.getSetting("deviceid_pause_on")
+		self.request(deviceid_on,True)
+		deviceid_off = settings.getSetting("deviceid_pause_off")
+		self.request(deviceid_off,False)
 
 	def onPlayBackResumed(self):
-		deviceid = settings.getSetting("deviceid_resume")
-		self.request(deviceid,False)
+		deviceid_on = settings.getSetting("deviceid_resume_on")
+		self.request(deviceid_on,True)
+		deviceid_off = settings.getSetting("deviceid_resume_off")
+		self.request(deviceid_off,False)
 
 	def onPlayBackEnded(self):
 		if playlist.size() == playlist.getposition() + 1:
-			deviceid = settings.getSetting("deviceid_end")
-			self.request(True)
+			deviceid_on = settings.getSetting("deviceid_end_on")
+			self.request(deviceid_on, True)
+			deviceid_off = settings.getSetting("deviceid_end_off")
+			self.request(deviceid_off, False)
 
 	def onPlayBackStopped(self):
-		deviceid = settings.getSetting("deviceid_stop")
-		self.request(deviceid, True)
+		deviceid_on = settings.getSetting("deviceid_stop_on")
+		self.request(deviceid_on, True)
+		deviceid_off = settings.getSetting("deviceid_stop_off")
+		self.request(deviceid_off, False)
 
 	def request(self,deviceid, switchOn):
+		if deviceid == 0 :
+			return
 		hostname = settings.getSetting("hostname")
 		port = settings.getSetting("port")
 
