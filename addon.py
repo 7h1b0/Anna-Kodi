a87c9a6195af9a5b61d9d4bbcf973bacf8336bdd
7h1b0 <wyrm1992@laposte.net>
2016-03-11 19:46:42 +0100

    Acts as a sensor

--- addon.py
+++ addon.py
@@ -16,38 +16,31 @@ class Anna(xbmc.Player):
 		pass
 
 	def onPlayBackStarted(self):
-		# if playlist.getposition() == 0:
-		sceneId = settings.getSetting("sceneid_on_start")
-		self.request(sceneId)
+		self.request("start")
 
 	def onPlayBackPaused(self):
-		sceneId = settings.getSetting("sceneid_on_pause")
-		self.request(sceneId)
+		self.request("pause")
 
 	def onPlayBackResumed(self):
-		sceneId = settings.getSetting("sceneid_on_resume")
-		self.request(sceneId)
+		self.request("resume")
 
 	def onPlayBackEnded(self):
 		if playlist.size() == playlist.getposition() + 1:
-			sceneId = settings.getSetting("sceneid_on_end")
-			self.request(sceneId)
+			self.request("ended")
 
 	def onPlayBackStopped(self):
-		sceneId = settings.getSetting("sceneid_on_stop")
-		self.request(sceneId)
+		self.request("stop")
 
-	def request(self,sceneId):
-		if sceneId == 0 :
-			return
+	def request(self, action):
 		hostname = settings.getSetting("hostname")
 		port = settings.getSetting("port")
 
-		print(hostname)
-		connection = httplib.HTTPConnection(hostname,port)
-		connection.connect()
-		url = "/scene/%s/action" % sceneId
+		connection = httplib.HTTPConnection(hostname, port)
+		token = settings.getSetting("token")
+		url = "/api/kodi/%s" % action
 
+		connection.connect()
+		connection.putheader("x-access-token", token)
 		connection.request("GET", url)
 
 	def execute(self):
