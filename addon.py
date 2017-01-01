955a1fc3c0db8467b93566ea3a123650458f83fc
7h1b0 <wyrm1992@laposte.net>
2017-01-01 01:50:52 +0100

    Custom Alias :+1:

--- addon.py
+++ addon.py
@@ -16,28 +16,33 @@ class Anna(xbmc.Player):
 		pass
 
 	def onPlayBackStarted(self):
-		self.request("kodi_on_play")
+		alias = settings.getSetting("onStart")
+		self.request(alias)
 
 	def onPlayBackPaused(self):
-		self.request("kodi_on_pause")
+		alias = settings.getSetting("onPause")
+		self.request(alias)
 
 	def onPlayBackResumed(self):
-		self.request("kodi_on_play")
+		alias = settings.getSetting("onResume")
+		self.request(alias)
 
 	def onPlayBackEnded(self):
 		if playlist.size() == playlist.getposition() + 1:
-			self.request("kodi_on_pause")
+			alias = settings.getSetting("onEnd")
+			self.request(alias)
 
 	def onPlayBackStopped(self):
-		self.request("kodi_on_pause")
+		alias = settings.getSetting("onStop")
+		self.request(alias)
 
-	def request(self, action):
+	def request(self, alias):
 		hostname = settings.getSetting("hostname")
 		port = settings.getSetting("port")
 
 		connection = httplib.HTTPConnection(hostname, port)
 		token = settings.getSetting("token")
-		url = "/api/alias/%s" % action
+		url = "/api/alias/%s" % alias
 
 		connection.putrequest("GET", url)
 		connection.putheader("x-access-token", token)
